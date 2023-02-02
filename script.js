43e377808d04afc4a931f3e2b8319c67bdcd0af3
dejourford <dejourford@my.unt.edu>
2023-02-01 23:12:01 -0600

    define variables

---
+++ script.js
@@ -0,0 +1,5 @@
+// define variables
+const jokeBtn = document.querySelector('.jokeBtn')
+const answerBtn = document.querySelector('.answerBtn')
+const joke = document.querySelector('.joke')
+const answer = document.querySelector('.answer')
