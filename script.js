6e8cd9a8bd1f7fefc69173703ab51d109ab1c8dd
dejourford <dejourford@my.unt.edu>
2023-02-01 23:41:04 -0600

    create function for joke button press

--- script.js
+++ script.js
@@ -4,22 +4,25 @@ const answerBtn = document.querySelector('.answerBtn')
 const joke = document.querySelector('.joke')
 const answer = document.querySelector('.answer')
 
-// fetch joke api
-fetch('https://official-joke-api.appspot.com/random_joke')
-.then(response => response.json())
-.then(data => {
-    console.log(data)
-    // event listener for joke button press
-    jokeBtn.addEventListener('click', function(){
-    joke.textContent = data.setup
 
-    // event listener for answer button press
-    answerBtn.addEventListener('click', function(){
+// event listener for joke button for new joke
+jokeBtn.addEventListener('click', function(){
+    // fetch joke api
+    fetch('https://official-joke-api.appspot.com/random_joke')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        // event listener for joke button press
+        joke.textContent = data.setup
+
+        // event listener for answer button press 
         answer.textContent = data.punchline
+        answer.style.visibility = 'hidden'
+        answerBtn.addEventListener('click', function(){
+            answer.style.visibility = 'visible'
+        })   
     })
-})    
+    .catch(error => {
+        console.log(`${error})`)
+    })
 })
-.catch(error => {
-    console.log(`${error})`)
-})
-
