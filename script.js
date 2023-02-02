30dbcd4bf114716320eb56675add658816179537
dejourford <dejourford@my.unt.edu>
2023-02-01 23:15:00 -0600

    fetch joke api

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const jokeBtn = document.querySelector('.jokeBtn')
 const answerBtn = document.querySelector('.answerBtn')
 const joke = document.querySelector('.joke')
 const answer = document.querySelector('.answer')
+
+// fetch joke api
+fetch('https://official-joke-api.appspot.com/random_joke')
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.log(`${error})`))
